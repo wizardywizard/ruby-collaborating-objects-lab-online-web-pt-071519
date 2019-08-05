5b8158f3ab54c1c799f88233f53c7c5e84881353
David Little <littledavid175@gmail.com>
2019-08-05 18:41:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,8 @@ class Song
     song
   end
   
-   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-  
+  def artist_name=(name)
+   self.artist = Artist.find_or_create_by_name(name)
+   self.artist.add_song(self)
+  end
 end
